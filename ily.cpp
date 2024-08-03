3c20ff484e648a2fd8b224ff11f789e2f547c781
russiandude07 <141868033+russiandudee@users.noreply.github.com>
2024-08-03 08:50:07 +0800

    Add files via upload

---
+++ ily.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "`````````````````````````" << endl;
+    cout << "`                       `" << endl;
+    cout << "`     I LOVE YOU        `" << endl;
+    cout << "`                       `" << endl;
+    cout << "`````````````````````````" << endl;
+    return 0;
+}
